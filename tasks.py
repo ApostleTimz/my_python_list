a7665de467605633cc2d9127ae9015432d2abd54
ApostleTimz <bulusakwashikitimothy@gmail.com>
2025-07-24 15:58:12 +0100

    python list hands on tasks

---
+++ tasks.py
@@ -0,0 +1,23 @@
+# 1. Create a list with 5 items
+market_list = ["tomatoes", "onions", "rice", "pepper", "yam"]
+
+
+print("Full market list:", market_list)
+
+
+print("First item:", market_list[0])
+print("Last item:", market_list[-1])
+
+
+market_list[2] = "beans"
+
+
+market_list.append("oil")
+
+
+market_list.pop(1)
+
+
+print("Updated market list:", market_list)
+print("Number of items:", len(market_list))
+
